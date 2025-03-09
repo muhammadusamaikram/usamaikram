e6f6d6d64b9c1d11f8db2d424f5eacfcf04a285d
Usama Ikram <your-email@example.com>
2025-03-09 13:09:02 +0500

    Initial commit - Uploaded ASP.NET MVC Project

---
+++ Assignment1/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Assignment1.Data;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+        }
+    }
+}
---
+++ Assignment1/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using Assignment1.Models;
+
+namespace Assignment1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("ApplicationDbContext") { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
